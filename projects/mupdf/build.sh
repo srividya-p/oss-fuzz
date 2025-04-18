f1dc890b6701007130e78c2f3b95ce4edd41de91
Srividya Subramanian <ssubramanian@ethz.ch>
2025-04-18 19:38:25 +0200

    Update Dockerfile + build.sh for new harness

--- projects/mupdf/build.sh
+++ projects/mupdf/build.sh
@@ -18,12 +18,13 @@
 # supp_size is unused in harfbuzz so we will avoid it being unused.
 sed -i 's/supp_size;/supp_size;(void)(supp_size);/g' ./thirdparty/harfbuzz/src/hb-subset-cff1.cc
 
+# TODO: build all fuzzers here
 LDFLAGS="$CXXFLAGS" make -j$(nproc) HAVE_GLUT=no build=debug OUT=$WORK \
     $WORK/libmupdf-third.a $WORK/libmupdf.a
-fuzz_target=pdf_fuzzer
+fuzz_target=validate_signature_fuzzer
 
 $CXX $CXXFLAGS -std=c++11 -Iinclude \
-    $SRC/pdf_fuzzer.cc -o $OUT/$fuzz_target \
+    $SRC/validate_signature_fuzzer.cc -o $OUT/$fuzz_target \
     $LIB_FUZZING_ENGINE $WORK/libmupdf.a $WORK/libmupdf-third.a
 
 mv $SRC/{*.zip,*.dict,*.options} $OUT
